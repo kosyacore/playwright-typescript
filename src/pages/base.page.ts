6f2bf9dafbf171fdb75e719156b9f53025e8b135
Anton Klimko <klimkoanton94@gmail.com>
2024-09-15 17:55:39 +0200

    Add BasePage POM class

---
+++ src/pages/base.page.ts
@@ -0,0 +1,27 @@
+import { type Locator, type Page } from "@playwright/test";
+
+/**
+ * Base page which contains common elements for all pages
+ */
+export class BasePage {
+    /**
+     * Page elements
+     */
+    readonly page: Page;
+    readonly loginMenu: Locator;
+    readonly loggedUserButton: Locator;
+    readonly notificationMessage: Locator;
+
+    /**
+     * Constructor
+     * @param page - browser tab
+     */
+    constructor(page: Page) {
+        this.page = page;
+
+        // Elements locators
+        this.loginMenu = page.getByTestId("login-menu");
+        this.loggedUserButton = page.getByTestId("logged-user");
+        this.notificationMessage = page.getByTestId("notification-message");
+    }
+}
