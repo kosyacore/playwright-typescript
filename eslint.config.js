9814c9a34b3755115ca0a91b3bdc24e081dcf6bb
Anton Klimko <klimkoanton94@gmail.com>
2024-09-15 19:38:36 +0200

    Add eslint dependencies and plugins Add eslint.config.js config Fix downgraded typescript to version 5.5.4 for proper eslint support

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import playwright from "eslint-plugin-playwright";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/node_modules/", "report/", ".vscode/*", ".husky", "eslint.config.js"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+    "prettier",
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: "./",
+      },
+    },
+  },
+  {
+    ...playwright.configs["flat/recommended"],
+    files: ["tests/**"],
+  },
+  {
+    files: ["tests/**"],
+    rules: {
+      "playwright/expect-expect": [
+        "error",
+        {
+          assertFunctionNames: ["assertBoardCreated", "assertBoardDeleted", "assertBoardRenamed"],
+        },
+      ],
+    },
+  },
+];
