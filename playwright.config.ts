6ee29cd55c8de6af990e1dd8741ed3828e7acc27
Anton Klimko <klimkoanton94@gmail.com>
2024-09-16 18:41:57 +0200

    Add screenshot config in playwright.config.ts Cleanup playwright.config.ts config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -40,6 +33,8 @@ export default defineConfig({
     trace: "on-first-retry",
     /* Defines custom attribute name to be used in page.getByTestId() */
     testIdAttribute: "data-cy",
+    /* Capture screenshot after each test failure. */
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
@@ -58,32 +53,5 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
